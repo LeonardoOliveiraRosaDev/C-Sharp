31ffe05644a600468ac7e582ede4f0ac8c45feab
Leonardo Oliveira Rosa <leonardooliveirarosadev@hotmail.com>
2022-10-10 19:46:16 -0300

    10102022-19:46

---
+++ CSharp.Capitulo01.Variaveis/VariaveisForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace CSharp.Capitulo01.Variaveis
+{
+    partial class VariaveisForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.operaçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aritmeticasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resultadoListBox = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.operaçõesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1087, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // operaçõesToolStripMenuItem
+            // 
+            this.operaçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aritmeticasToolStripMenuItem});
+            this.operaçõesToolStripMenuItem.Name = "operaçõesToolStripMenuItem";
+            this.operaçõesToolStripMenuItem.Size = new System.Drawing.Size(94, 24);
+            this.operaçõesToolStripMenuItem.Text = "Operações";
+            // 
+            // aritmeticasToolStripMenuItem
+            // 
+            this.aritmeticasToolStripMenuItem.Name = "aritmeticasToolStripMenuItem";
+            this.aritmeticasToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aritmeticasToolStripMenuItem.Text = "Aritmeticas";
+            this.aritmeticasToolStripMenuItem.Click += new System.EventHandler(this.aritméticasToolStripMenuItem_Click);
+            // 
+            // resultadoListBox
+            // 
+            this.resultadoListBox.FormattingEnabled = true;
+            this.resultadoListBox.ItemHeight = 16;
+            this.resultadoListBox.Location = new System.Drawing.Point(12, 31);
+            this.resultadoListBox.Name = "resultadoListBox";
+            this.resultadoListBox.Size = new System.Drawing.Size(1024, 644);
+            this.resultadoListBox.TabIndex = 1;
+            // 
+            // VariaveisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 686);
+            this.Controls.Add(this.resultadoListBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "VariaveisForm";
+            this.Text = "Variáveis e Operações";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem operaçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aritmeticasToolStripMenuItem;
+        private System.Windows.Forms.ListBox resultadoListBox;
+    }
+}
+
---
+++ CSharp.Capitulo01.Variaveis/VariaveisForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp.Capitulo01.Variaveis
+{
+    public partial class VariaveisForm : Form
+    {
+        public VariaveisForm()
+        {
+            InitializeComponent();
+        }
+
+        private void aritméticasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // criar uma variavel tipo inteiro
+            int x = 42;
+            x = -9;
+            //x = "58"
+            int meuInteiro = 10;
+            string nome = "Leonardo";
+            char letra = 'L';
+            DateTime dataNascimento = new DateTime(1981, 5, 20);
+            bool aprovado = true;
+            decimal valor = 22.09m;
+
+            object meuObjeto = 18;
+            meuObjeto = "35";
+            meuObjeto = false;
+            meuObjeto = new DateTime(1981, 5, 20);
+
+            //decimal 1bimestre = 8.5m;
+            var nomeCancao = "Release";
+            string @class ="5D";
+            var a = 2;
+            var b = 6;
+            var c = 10;
+            var d = 13;
+
+            int f;
+
+            resultadoListBox.Items.Add("a = "+ a);
+            // outra forma de fazer concatenação ! usando o String.Concat() 
+            //cstr() converter para string
+            resultadoListBox.Items.Add(string.Concat("b = ",b));
+            resultadoListBox.Items.Add(string.Format("c = {0}",c));
+            resultadoListBox.Items.Add();
+        }
+    }
+}
