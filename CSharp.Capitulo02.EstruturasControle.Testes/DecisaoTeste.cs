349d27c59086ca268f3a3dbb8966c1ee32b709c0
Leonardo Oliveira Rosa <leonardooliveirarosadev@hotmail.com>
2022-10-14 21:36:19 -0300

    14102022-21:10-Parte01

---
+++ CSharp.Capitulo02.EstruturasControle.Testes/DecisaoTeste.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Capitulo02.EstruturasControle.Testes
+{
+    [TestClass]
+    public class DecisaoTeste
+    {
+        [TestMethod]
+        public void AvaliacaoFinalReprovadoTeste()
+        {
+            var notaFinal = 2.2d;
+            string resultadoFinal = string.Empty;
+
+            if (notaFinal < 3)
+            {
+                resultadoFinal = "Reprovado";
+            }
+             else if (notaFinal is > 3 and < 5)
+            {
+                resultadoFinal = "Recuperação";
+            }
+            //if (notaFinal > 5)
+            else
+            {
+                resultadoFinal = "Aprovado";
+            }
+
+            Assert.AreEqual(resultadoFinal, "Reprovado");
+        }
+        [TestMethod]
+        public void AvaliacaoFinalRecuperacao3Teste()
+        {
+            var notaFinal = 3.0;
+            var resultadoFinal = string.Empty;
+            switch (notaFinal)
+            {
+                default:
+            }
+        }
+    }
+}
