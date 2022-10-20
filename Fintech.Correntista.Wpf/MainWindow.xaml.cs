f9d4fe21d1478d3067e3f22341a483606d11cf06
Leonardo Oliveira Rosa <leonardooliveirarosadev@hotmail.com>
2022-10-19 23:02:03 -0300

    inclusao de cliente

---
+++ Fintech.Correntista.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Fintech.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fintech.Correntista.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopularControles();
+        }
+
+        private void PopularControles()
+        {
+            sexoComboBox.Items.Add(Sexo.Feminino);
+            sexoComboBox.Items.Add(Sexo.Masculino);
+            sexoComboBox.Items.Add(Sexo.Outro);
+        }
+
+        private void incluirClienteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Cpf = cpfTextBox.Text;
+            cliente.DataNascimento = Convert.ToDateTime(dataNascimentoTextBox.Text);
+            cliente.Nome = nomeTextBox.Text;
+            cliente.Sexo = (Sexo)sexoComboBox.SelectedItem;
+
+            Endereco endereco = new Endereco();
+            endereco.Cep = cepTextBox.Text;
+            endereco.Cidade = cidadeTextBox.Text;
+            endereco.Logradouro = logradouroTextBox.Text;
+            endereco.Numero = Convert.ToInt32(numeroLogradouroTextBox.Text);
+            endereco.Complemento = complementoTextBox.Text;
+
+            cliente.EnderecoResidencial = endereco;
+        }
+    }
+}
